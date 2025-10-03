102177e82d403f9ad89eb0eb6504c4c44d9ffb10
Lubos <sytensky@gmail.com>
2025-10-03 13:43:15 +0200

    Add Compose conventional plugin

--- src/main/kotlin/cz/yogaboy/build/AndroidLibraryConventionPlugin.kt
+++ src/main/kotlin/cz/yogaboy/build/AndroidLibraryConventionPlugin.kt
@@ -1,15 +1,14 @@
 package cz.yogaboy.build
 
-import com.android.build.gradle.LibraryExtension
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.JavaVersion
 import org.gradle.kotlin.dsl.configure
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.pluginManager.apply("com.android.library")
-        target.pluginManager.apply("org.jetbrains.kotlin.android")
         target.extensions.configure<LibraryExtension> {
             compileSdk = 35
             defaultConfig {
